039333b6d1f8ca903cccd9032cb62b8e6b987bde
Brenda <113529315+Brendaur11@users.noreply.github.com>
2024-12-03 20:50:10 +0000

    Commit

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,12 +1,18 @@
+import { useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({category}) => {
+
+  const [counter, setCounter] = useState(10)
 
   getGifs(category);
 
   return (
     <>
-        <h2>{category}</h2>
+      <h2>{category}</h2>
+
+      <h5>{counter}</h5>
+      <button onClick={() => setCounter(counter + 1)}>+1</button>
     </>
   )
 }
--- gif-expert-app/src/helpers/getGifs.js
+++ gif-expert-app/src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async(category) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=OVD0O2W9LMdoi4HtPPqQHZkdWmnL52Ct&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=OVD0O2W9LMdoi4HtPPqQHZkdWmnL52Ct&q=${category}&limit=10`;
     const resp = await fetch (url);
     const {data} = await resp.json();
 
