04ed6d8659add5fa88f2ef331f6b0cbe7ce251f6
Brenda <113529315+Brendaur11@users.noreply.github.com>
2024-12-04 20:53:07 +0000

    Commit

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({category}) => {
 
   const [counter, setCounter] = useState(10)
 
-  getGifs(category);
+  useEffect(() => {
+    getGifs(category);
+  }, [ ])
 
   return (
     <>
