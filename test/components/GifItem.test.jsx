5ed760862f629e1bfe6df2b4e8d55b258d6bd08c
Brenda <113529315+Brendaur11@users.noreply.github.com>
2024-12-30 20:36:01 +0000

    Commit

---
+++ test/components/GifItem.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react"
+import { GifItem } from "../../src/components/GifItem"
+
+describe('Pruebas en GifItem', () => { 
+
+    const title = 'Futurama'
+    const url = 'https://Futurama.com.ar/futurama.jpg'
+
+    test('debe hacer match con el Snapshot', () => { 
+    
+        const {container} = render(<GifItem title={title} url={url}/>)
+    
+        expect(container).toMatchSnapshot();
+    });
+
+    test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+        render(<GifItem title={title} url={url}/>);
+        // screen.debug();
+        const {src, alt} = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(alt);
+    });
+
+});
+
